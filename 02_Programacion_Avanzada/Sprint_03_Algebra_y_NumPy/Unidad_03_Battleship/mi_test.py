400b1fed221d0a879aaf00b06c49a1c18ad421c7
Elecomexp <lander.combarro@outlook.com>
2024-06-19 17:26:43 +0200

    Team Challenge - Battleship

---
+++ 02_Programacion_Avanzada/Sprint_03_Algebra_y_NumPy/Unidad_03_Battleship/mi_test.py
@@ -0,0 +1,10 @@
+import mi_test2
+
+print('hola mundo')
+
+leido = int(input('Introduce un número:')) + 2
+print('Resultado: ', leido)
+
+print(mi_test2.color)
+
+mi_test2.saluda()
---
+++ 02_Programacion_Avanzada/Sprint_03_Algebra_y_NumPy/Unidad_03_Battleship/mi_test2.py
@@ -0,0 +1,5 @@
+def saluda():
+    print('Ey!')
+
+color = 'rojo'
+
