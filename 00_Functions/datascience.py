47da8463dcf4d5ea9fa7d56ad174ec090ed7dc84
Elecomexp <lander.combarro@outlook.com>
2024-07-10 18:20:14 +0200

    My own functions

---
+++ 00_Functions/datascience.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def get_cardinality_class(df_in, umbral_categoria, umbral_continua):
+    '''
+    Define qué tipo de variable es cada columna de un pandas.DataFrame en función de su cardinalidad.
+    '''
+    df_out = pd.DataFrame([df_in.nunique(), df_in.nunique()/len(df_in) * 100, df_in.dtypes])
+    df_out = df_out.T.rename(columns = {0: "Card", 1: "%_Card", 2: "Tipo"})
+    
+
+    df_out.loc[df_out["Card"] < umbral_categoria, "Clase"] = "Categórica"    
+    df_out.loc[df_out["Card"] == 2, "Clase"] = "Binaria"
+    df_out.loc[df_out["Card"] >= umbral_categoria, "Clase"] ="Numérica Discreta"
+    df_out.loc[df_out["%_Card"] > umbral_continua, "Clase"] = "Numérica Continua"
+    
+    return df_out
