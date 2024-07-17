4e3b6968b6b52011b609ec4c4b8c82aab00aab05
Elecomexp <lander.combarro@outlook.com>
2024-07-17 09:39:36 +0200

    Update bootcampviztools.py

--- 00_Functions/bootcampviztools.py
+++ 00_Functions/bootcampviztools.py
@@ -4,9 +4,10 @@ Date: 2024-07-17
 '''
 
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import squarify
 
 '''
 ###################################################################################################
