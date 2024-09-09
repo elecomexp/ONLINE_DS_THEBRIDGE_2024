2225ef7848bce1dcfc3dbeed9942b121447b5742
Lander <lander.combarro@outlook.com>
2024-09-09 18:08:31 +0200

    Update datascience.py

--- 00_Functions/datascience.py
+++ 00_Functions/datascience.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 import scipy.stats
 
-def get_cardinality(df : pd.DataFrame, threshold_categorical=10, threshold_continuous=30):
+def get_cardinality(df: pd.DataFrame, threshold_categorical=10, threshold_continuous=30):
     '''
     Calculates and returns cardinality statistics for each column in a pandas DataFrame, 
     classifying the columns based on their cardinality.
@@ -15,9 +15,11 @@ def get_cardinality(df : pd.DataFrame, threshold_categorical=10, threshold_conti
     ----------
     df : pandas.DataFrame
         The input DataFrame for which cardinality statistics will be computed.
+    
     threshold_categorical : int, optional (default=10)
         The threshold used to classify columns as 'Categoric' or 'Numeric - Discrete'. 
         Columns with a number of unique values less than this threshold are classified as 'Categoric'.
+    
     threshold_continuous : int, optional (default=30)
         The threshold percentage used to classify columns as 'Numeric - Continuous'. 
         Columns where the percentage of unique values exceeds this threshold are classified as 'Numeric - Continuous'.
@@ -29,20 +31,24 @@ def get_cardinality(df : pd.DataFrame, threshold_categorical=10, threshold_conti
         - 'Card': The number of unique values in each column.
         - '%_Card': The percentage of unique values relative to the total number of rows in each column.
         - 'NaN_Values': The number of missing (NaN) values in each column.
+        - '%_NaN_Values': The percentage of missing values relative to the total number of rows in each column.
         - 'Type': The data type of each column.
         - 'Class': The classification of each column based on its cardinality.
     '''
     print('pandas.DataFrame shape: ', df.shape)
     
-    df_out = pd.DataFrame([df.nunique(), df.nunique()/len(df) * 100, df.isna().sum(), df.dtypes])
-    df_out = df_out.T.rename(columns = {0: 'Card', 1: '%_Card', 2: 'NaN_Values', 3: 'Type'})
+    df_out = pd.DataFrame([df.nunique(), df.nunique()/len(df) * 100, 
+                           df.isna().sum(), df.isna().mean() * 100, df.dtypes])
+    df_out = df_out.T.rename(columns={0: 'Card', 1: '%_Card', 
+                                      2: 'NaN_Values', 3: '%_NaN_Values', 4: 'Type'})
     
-    df_out.loc[df_out['Card'] < threshold_categorical, 'Class'] = 'Categoric'    
+    df_out.loc[df_out['Card'] < threshold_categorical, 'Class'] = 'Categoric'
     df_out.loc[df_out['Card'] == 2, 'Class'] = 'Binary'
-    df_out.loc[df_out['Card'] >= threshold_categorical, 'Class'] ='Numeric - Discrete'
+    df_out.loc[df_out['Card'] >= threshold_categorical, 'Class'] = 'Numeric - Discrete'
     df_out.loc[df_out['%_Card'] > threshold_continuous, 'Class'] = 'Numeric - Continuous'
     
     return df_out
+
 
 
 def coeficiente_variación(df):
