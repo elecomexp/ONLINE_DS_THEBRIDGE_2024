4bd8a0edc022466956d5608951bdaab195563f8d
Lander <lander.combarro@outlook.com>
2024-09-27 20:35:24 +0200

    S14 Team Challenge update

--- 05_Machine_Learning/S14_Supervisados_III/Team_Challenge_Kaggle/src/utils/functions.py
+++ 05_Machine_Learning/S14_Supervisados_III/Team_Challenge_Kaggle/src/utils/functions.py
@@ -1,7 +1,91 @@
+import re
+
+import numpy as np
 import pandas as pd
 import urllib.request
 
 from PIL import Image
+
+
+# Suponiendo que df_train_mod es tu DataFrame
+def extract_cpu_features(cpu):
+    # Inicializar valores
+    brand = None
+    family = None
+    cores = None
+    frequency = None
+    
+    # Extraer la marca
+    if 'Intel' in cpu:
+        brand = 'Intel'
+    elif 'AMD' in cpu:
+        brand = 'AMD'
+    
+    # Extraer la familia (i3, i5, i7, etc.)
+    match = re.search(r'(i[357])|AMD (A[6-9]|FX|A10|A12)', cpu)
+    if match:
+        family = match.group(0)
+    
+    # Extraer la frecuencia
+    match = re.search(r'(\d+\.\d+|\d+)GHz', cpu)
+    if match:
+        frequency = float(match.group(1))
+    
+    return pd.Series([brand, family, cores, frequency])
+
+
+
+def extract_screen_features(screen_resolution):
+    """
+    Desglosa la columna "ScreenResolution" en nuevas características que representan la resolución, 
+    el tipo de panel y la presencia de la función táctil (touchscreen).
+    """
+    # Inicializar valores
+    width = None
+    height = None
+    is_ips = 0
+    is_retina = 0
+    is_touchscreen = 0
+    
+    # Buscar el ancho y la altura
+    match = re.search(r'(\d+)x(\d+)', screen_resolution)
+    if match:
+        width = int(match.group(1))
+        height = int(match.group(2))
+    
+    # Verificar si es un IPS Panel
+    if 'IPS Panel' in screen_resolution:
+        is_ips = 1
+    
+    # Verificar si es un Retina Display
+    if 'Retina Display' in screen_resolution:
+        is_retina = 1
+
+    # Verificar si tiene Touchscreen
+    if 'Touchscreen' in screen_resolution:
+        is_touchscreen = 1
+    
+    return pd.Series([width, height, is_ips, is_retina, is_touchscreen])
+
+def extract_memory_by_type(storage, storage_type):
+    """
+    Función para extraer la capacidad en GB por tipo de almacenamiento
+    """
+    capacity = 0
+    for part in storage.split('+'):
+        part = part.strip()
+        if storage_type in part:
+            # Buscar si tiene capacidad en GB o TB usando regex
+            gb_match = re.search(r'(\d+)\s*GB', part)
+            tb_match = re.search(r'(\d+(\.\d+)?)\s*TB', part)
+            
+            if gb_match:
+                capacity += int(gb_match.group(1))  # Extraer valor en GB
+            elif tb_match:
+                capacity += int(float(tb_match.group(1)) * 1000)  # Convertir TB a GB
+    return capacity
+
+
 
 def kaggle_checker(df_to_submit, path, sample=pd.read_csv(r'./data/sample_submission.csv')):
     """
