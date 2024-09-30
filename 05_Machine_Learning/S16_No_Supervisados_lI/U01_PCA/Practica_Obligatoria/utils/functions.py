24105fcc863c646f05fadaa334d58e7839494f62
Lander <lander.combarro@outlook.com>
2024-09-30 22:08:57 +0200

    S16 U01

---
+++ 05_Machine_Learning/S16_No_Supervisados_lI/U01_PCA/Practica_Obligatoria/utils/functions.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def plot_faces(faces, labels, n_cols=10):
+    faces = faces.reshape(-1, 64, 64)
+    n_rows = (len(faces) - 1) // n_cols + 1
+    plt.figure(figsize=(n_cols, n_rows * 1.1))
+    for index, (face, label) in enumerate(zip(faces, labels)):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(face, cmap="gray")
+        plt.axis("off")
+        plt.title(label)
+    plt.show()
