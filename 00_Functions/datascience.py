bf3d3f5efe8582b558e31ab1dada0059b6ff79b1
Lander <lander.combarro@outlook.com>
2024-11-13 15:35:40 -0600

    Update datascience.py

--- 00_Functions/datascience.py
+++ 00_Functions/datascience.py
@@ -1,13 +1,28 @@
+"""
+datascience.py
+
+Author: Lander Combarro Exposito
+Created: 2024/07/17
+Last Modified: 2024/11/18
+
+Description
+-----------
+Module for Data Analysis Utilities.
+
+This module provides a set of utility functions for data analysis, including calculating 
+the coefficient of variation, generating heatmaps of correlation matrices, and calculating 
+cardinality statistics for DataFrame columns.
+"""
+
 import re
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-import scipy.stats
 
 def get_cardinality(df: pd.DataFrame, threshold_categorical=10, threshold_continuous=30):
-    '''
+    """
     Calculates and returns cardinality statistics for each column in a pandas DataFrame, 
     classifying the columns based on their cardinality.
 
@@ -34,7 +49,7 @@ def get_cardinality(df: pd.DataFrame, threshold_categorical=10, threshold_contin
         - '%_NaN_Values': The percentage of missing values relative to the total number of rows in each column.
         - 'Type': The data type of each column.
         - 'Class': The classification of each column based on its cardinality.
-    '''
+    """
     print('pandas.DataFrame shape:', df.shape)
     
     df_out = pd.DataFrame([df.nunique(), df.nunique()/len(df) * 100, 
@@ -50,108 +65,80 @@ def get_cardinality(df: pd.DataFrame, threshold_categorical=10, threshold_contin
     return df_out
 
 
+def coefficient_of_variation(df):
+    """
+    Returns a pandas DataFrame with the mean, standard deviation (std), 
+    in the same units as the mean, and the coefficient of variation (CV) for each numerical column in the input DataFrame.
 
-def coeficiente_variación(df):
-    '''
-    Devuelve un pandas.DataFrame con la media, la desviación estándar (ro), 
-    en las mismas unidades que la media y su coeficiente de variación (CV)
-    '''
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A DataFrame containing numerical columns for which the mean, standard deviation, 
+        and coefficient of variation are calculated.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame with the following columns:
+        - 'mean': The mean of each column.
+        - 'std': The standard deviation of each column.
+        - 'CV': The coefficient of variation, calculated as the ratio of standard deviation to mean.
+        
+    Notes
+    -----
+    The coefficient of variation (CV) is a normalized measure of the dispersion of a dataset, 
+    which is useful for comparing the variability of different datasets with different units or scales.
+    """
     df_var = df.describe().loc[['std', 'mean']].T
     df_var['CV'] = df_var['std'] / df_var['mean']
     return df_var
 
 
 def split_by_uppercase(text) -> str:
-    '''
+    """
     Uses regular expressions to find uppercase letters, split a string at those points,
     and return a new string separated by spaces.
     
-    Parameters:
+    Parameters
     ----------
     text : str
         Text to split.
         
-    Returns:
+    Returns
     -------
     str
         The modified string with spaces inserted before each uppercase letter.
-    '''
+    """
     strings = re.findall(r'[A-Z][^A-Z]*', text)
     return ' '.join(strings)
 
 
-# scipy.stats.mannwhitneyu()
-
-# scipy.stats.f_oneway()
-
-def mapa_calor(corr_matrix):
-    '''
-    Hay que introducir una matriz de correlación generada con pandas
-    '''
-    plt.figure(figsize=(10, 8))  # Ya lo veremos pero esto permite ajustar el tamaño de las gráficas
-    sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm", 
-                cbar=True, square=True, linewidths=.5) # el cmap es el rango de colores usado para representar "el calor"
-
-    plt.title('Matriz de Correlación')
-    plt.xticks(rotation=45)  # Rota las etiquetas de las x si es necesario
-    plt.yticks(rotation=45)  # Rota las etiquetas de las y si es necesario
-
-    plt.show()
-    
-    
-
-
-
-
-
-
-
-
-###########################################################################################
-###     DEPRECATED      DEPRECATED      DEPRECATED      DEPRECATED      DEPRECATED      ###
-###########################################################################################
-
-
-def get_cardinality_class(df_in, threshold_categorical = 10, threshold_continuous = 30):
-    '''
-    Categorizes each column of `df_in` in a pandas.DataFrame based on its cardinality.
+def correlation_heatmap(corr_matrix):
+    """
+    Generates and displays a heatmap for the given correlation matrix using seaborn.
 
     Parameters
     ----------
-    df_in : pandas.DataFrame
-        The input DataFrame containing the data to be analyzed.
-        
-    threshold_categorical : int, optional
-        The threshold for determining categorical variables. Columns with unique values 
-        less than this threshold are considered categorical. Default is 10.
-        
-    threshold_continuous : int, optional
-        The threshold percentage for determining continuous numerical variables. Columns with a 
-        cardinality percentage higher than this threshold are considered continuous numerical. Default is 30.
+    corr_matrix : pandas.DataFrame
+        A correlation matrix, typically generated using pandas' `DataFrame.corr()` method, 
+        where the values represent the correlation coefficients between different variables.
 
     Returns
     -------
-    pandas.DataFrame with columns:
-        - 'Card': The cardinality of each column.
-        - '%_Card': The percentage cardinality of each column relative to the total number of rows.
-        - 'Tipo': The data type of each column.
-        - 'Clase': The assigned variable class for each column based on the specified thresholds.
+    None
+        This function directly displays a heatmap using matplotlib.
 
     Notes
     -----
-    The function assigns variable classes as follows:
-        - 'Categoric' for columns with cardinality less than `threshold_categorical`.
-        - 'Binary' for columns with exactly 2 unique values.
-        - 'Numeric - Discrete' for columns with cardinality greater than or equal to `threshold_categorical`.
-        - 'Numeric - Continuous' for columns with a cardinality percentage greater than `threshold_continuous`.
-    '''
-    df_out = pd.DataFrame([df_in.nunique(), df_in.nunique()/len(df_in) * 100, df_in.dtypes])
-    df_out = df_out.T.rename(columns = {0: "Card", 1: "%_Card", 2: "Tipo"})
-    
+    - The `annot=True` argument in `sns.heatmap` annotates each cell with the numeric value of the correlation.
+    - The `cmap="coolwarm"` argument defines the color palette used to represent the values, ranging from cool (blue) to warm (red) colors.
+    - The heatmap is displayed with 45-degree rotated labels on both axes for better readability when needed.
+    """
+    plt.figure(figsize=(10, 8)) 
+    sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm", 
+                cbar=True, square=True, linewidths=.5)
 
-    df_out.loc[df_out["Card"] < threshold_categorical, "Clase"] = "Categoric"    
-    df_out.loc[df_out["Card"] == 2, "Clase"] = "Binary"
-    df_out.loc[df_out["Card"] >= threshold_categorical, "Clase"] ="Numeric - Discrete"
-    df_out.loc[df_out["%_Card"] > threshold_continuous, "Clase"] = "Numeric - Continuous"
-    
-    return df_out
+    plt.title('Correlation Matrix')
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=45)
+    plt.show()
